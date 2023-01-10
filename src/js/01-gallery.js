149acdec88d80e93277f6e3f91d9bb9362ca5c0c
Dimon <bolilui.dmytro@gmail.com>
2023-01-10 11:52:47 +0200

    HW8 - Task1 - Done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ! 1 - Creation and rendering markup
+
+// * get gallery container
+const galleryEl = document.querySelector('.gallery');
+
+// * Function - create gallery item
+function createGalleryItem(item) {
+  const { preview, original, description } = item;
+  let itemToInsert = `
+      <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title = "${description}"
+      />
+    </a>
+  `;
+
+  return itemToInsert;
+}
+
+// * Container for all elements
+const imagesWrapper = galleryItems.map(createGalleryItem).join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', imagesWrapper);
+
+// ! 2 - Prevent default behavior for the link element, SimpleLightbox
+
+// * use SimpleLightbox library
+let lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionDelay: 300,
+});
+
+// * Prevent default behavior for the links <a>
+const links = document.querySelectorAll('.gallery__link');
+links.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+  });
+});
