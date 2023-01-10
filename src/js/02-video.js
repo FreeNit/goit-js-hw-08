2b4e01b1c6c7e5cc330ee1a6aff68989b8d6418c
Dimon <bolilui.dmytro@gmail.com>
2023-01-10 16:18:44 +0200

    HW8 - Task2 - Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,46 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const playerEl = document.querySelector('#vimeo-player');
+
+const player = new Player(playerEl);
+
+player.on(
+  'timeupdate',
+  // use throttle in order to call a function one time per 1000ms
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  }, 1000)
+);
+
+// timestamp - where the video should continue to play after page reload
+let playFromSecond;
+
+try {
+  // get local storage data and parse it
+  const playerData = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  // get the current video`s timestamp
+  playFromSecond = playerData.seconds;
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
+
+player
+  .setCurrentTime(playFromSecond)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
